226cec17aae3d10206f4d924f42efa9017507a59
Šimon Brauner <simonbrauner@seznam.cz>
2026-05-08 20:47:54 +0200

    Add in-memory counter

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+count = 0
+
 @app.route("/")
 def counter():
-    return render_template("index.html")
+    global count
+    count += 1
+    return render_template("index.html", count=count)
